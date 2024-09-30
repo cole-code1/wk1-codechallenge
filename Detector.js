b269f69d31cd6eb489d56f6225993d141529305d
Collins Kathurima <collinskathu313@gmail.com>
2024-09-30 08:55:03 +0300

    complete code challenge

---
+++ Detector.js
@@ -0,0 +1,12 @@
+// speed detector is a program used to print speed of a car
+let Speeed = 180
+if (Speeed < 70) {
+    console.log("Ok");
+}
+else if(Speeed = 80){
+    console.log("Points:2");
+    
+}
+else if (Speeed >= 180){
+    console.log("License suspended");
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+//  This is aprogram used to input student Marks
+let Grade = 100
+if (Grade>=79) {
+    console.log("A");
+}
+else if(Grade>=60 && Grade<=79){
+    console.log("B");
+}
+else if(Grade>=49 && Grade<=59){
+    console.log("C");
+}
+else if(Grade>=40 && Grade<=49){
+    console.log("D");
+}
+else if(Grade<40){
+    console.log("E");
+}
